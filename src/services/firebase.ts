fe53a85d02108c14c35824bbfbc405d10b648426
gabvask <gabrielvascoauzier@gmail.com>
2022-06-13 20:34:21 -0400

    build: update firebase config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,12 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // your firebase config
+  apiKey: "AIzaSyCKSSvvMf1CT7ElwLQQIgwaA0MtaIgdew0",
+  authDomain: "telzir-353100.firebaseapp.com",
+  projectId: "telzir-353100",
+  storageBucket: "telzir-353100.appspot.com",
+  messagingSenderId: "296266684564",
+  appId: "1:296266684564:web:bec885e3692b55551cbc81",
+  measurementId: "G-EGRHRGQLHN",
 };
 
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
 
+export const database = getFirestore(app);
 export const auth = getAuth(app);
